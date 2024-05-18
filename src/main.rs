7fbce557e84aadef4e72bbccd21dadaeec007228
jcha0713 <joocha0713@gmail.com>
2024-05-18 10:34:28 +0900

    Refactor using struct

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,37 @@ use std::io::{self, Write};
 use std::thread;
 use std::time::{Duration, Instant};
 
-fn main() {
-    countdown(10);
+pub struct Timer {
+    start: Instant,
+    remaining: u64,
 }
 
-fn countdown(t: u64) {
-    let now = Instant::now();
-    while now.elapsed().as_secs() <= t {
-        let remaining = t - now.elapsed().as_secs();
-        let hours = remaining / 3600;
-        let minutes = (remaining % 3600) / 60;
-        let seconds = remaining % 60;
-        print!("\r{:02}:{:02}:{:02}", hours, minutes, seconds);
-        io::stdout().flush().unwrap();
-        thread::sleep(Duration::from_millis(1000));
+impl Timer {
+    pub fn new(t: u64) -> Self {
+        Self {
+            start: Instant::now(),
+            remaining: t,
+        }
     }
-    println!();
+
+    pub fn countdown(&self) {
+        while self.start.elapsed().as_secs() <= self.remaining {
+            let remaining = self.remaining - self.start.elapsed().as_secs();
+            let hours = remaining / 3600;
+            let minutes = (remaining % 3600) / 60;
+            let seconds = remaining % 60;
+            print!("\r{:02}:{:02}:{:02}", hours, minutes, seconds);
+            io::stdout().flush().unwrap();
+            thread::sleep(Duration::from_millis(1000));
+        }
+    }
+}
+
+fn main() {
+    let timer = Timer {
+        start: Instant::now(),
+        remaining: 1000,
+    };
+
+    timer.countdown();
 }
