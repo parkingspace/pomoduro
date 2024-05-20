d338f5f6fc16d03288ab5455dab4698f273db306
jcha0713 <joocha0713@gmail.com>
2024-05-20 19:45:21 +0900

    Add clap library

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,46 @@
+/*
+TODO:
+- extract Timer into its own module
+- add unit tests
+- Clap
+- Ratatui
+*/
+use clap::Parser;
 use core::fmt;
 use std::io::{self, Write};
 use std::thread;
 use std::time::{Duration, Instant};
 
+/**
+By default the time argument is in minutes.
+It can be used in seconds, days, or hours.
+
+For example, 300s counts 5 minutes and 24h counts 1440 minutes.
+*/
+#[derive(Parser)]
+#[command(name = "Pomodoro Timer")]
+#[command(version = "0.1")]
+#[command(about = "Time", long_about = None)]
+struct Cli {
+    #[arg(short, long)]
+    duration: u64,
+}
+
 pub struct Timer {
     start: Instant,
-    remaining: u64,
+    duration: u64,
 }
 
 impl Timer {
     pub fn new(t: u64) -> Self {
         Timer {
             start: Instant::now(),
-            remaining: t,
+            duration: t,
         }
     }
 
     pub fn countdown(&self) {
-        while self.start.elapsed().as_secs() <= self.remaining {
+        while self.start.elapsed().as_secs() <= self.duration {
             print!("\r{}", self);
             io::stdout().flush().unwrap();
             thread::sleep(Duration::from_millis(1000));
@@ -25,21 +48,43 @@ impl Timer {
     }
 }
 
+// FIX: timer doesn't flush out correctly when it's more than an 1 day
+// TODO: handle singular/plural cases
 impl fmt::Display for Timer {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let remaining = self.remaining - self.start.elapsed().as_secs();
-        let hours = remaining / 3600;
-        let minutes = (remaining % 3600) / 60;
-        let seconds = remaining % 60;
-        write!(f, "{:02}:{:02}:{:02}", hours, minutes, seconds)
+        let duration = self.duration - self.start.elapsed().as_secs();
+        match duration {
+            0..=3600 => {
+                let minutes = (duration % 3600) / 60;
+                let seconds = duration % 60;
+                write!(f, "{:02}:{:02}", minutes, seconds)
+            }
+            3601..=86400 => {
+                let hours = duration / 3600;
+                let minutes = (duration % 3600) / 60;
+                let seconds = duration % 60;
+                write!(f, "{:02}:{:02}:{:02}", hours, minutes, seconds)
+            }
+            _ => {
+                let days = duration / 86400;
+                let hours = (duration % 86400) / 3600;
+                let minutes = (duration % 3600) / 60;
+                let seconds = duration % 60;
+                write!(
+                    f,
+                    "{} days, {:02}:{:02}:{:02}",
+                    days, hours, minutes, seconds
+                )
+            }
+        }
     }
 }
 
 fn main() {
-    let timer = Timer {
-        start: Instant::now(),
-        remaining: 1000,
-    };
+    let cli = Cli::parse();
 
+    let time_in_minutes = cli.duration * 60;
+
+    let timer = Timer::new(time_in_minutes);
     timer.countdown();
 }
